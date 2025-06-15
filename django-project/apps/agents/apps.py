3c8bdc1cc3f1cfa578ba0a08f40a77c79a7f1b04
LOYSURYYA_Punh <147781030+PPunh@users.noreply.github.com>
2025-06-15 22:20:37 +0700

    Delete django-project/apps/agents directory

--- django-project/apps/agents/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-# make sure to update AppClassName and App name
-class AppBoilerplateSmallConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = 'apps.agents'
-    verbose_name = 'Insurance Agents'
-    label = 'agents'
